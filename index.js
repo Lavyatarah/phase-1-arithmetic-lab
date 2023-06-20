727a19d11d83205ca9a27412ef72989a9b824bc9
lavyatarah <mattlavyy@gmail.com>
2023-06-20 19:35:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Write your code herelet 
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 5) + 1;
+
+
+let num3 = 18;
+let num4 = 7;
+let mod = num3 % num4;
+
+
+let max = Math.max(5, 9, 11, 12, 14, 16, 20);
+
+console.log("Multiply:", multiply);
+console.log("Random:", random);
+console.log("Mod:", mod);
+console.log("Max:", max);
